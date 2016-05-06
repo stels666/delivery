ce2f3d49f96594bd227869d70d3038d5e9150afd
Andrew Radetsky <svisloch.stels@gmail.com>
2016-05-06 19:47:24 +0300

    initializer

--- server/index.js
+++ server/index.js
@@ -1,17 +1,21 @@
 var mongoose = require('lib/mongoose'),
-    server = require('lib/server')(__dirname);
+    server = require('lib/server')(__dirname),
+    initializer = require('lib/initializer');
 
-server.run()
-
-    .then(function(){
-        return mongoose.reset();
-    })
+mongoose.reset()
 
     .then(function(models){
+        return initializer.defaultDBState(models);
+    })
 
+    .then(function(){
+        return server.run();
     })
 
     .catch(function(error) {
         mongoose.disconect();
         server.shutdown();
     });
+
+
+
---
+++ server/lib/initializer.js
@@ -0,0 +1,54 @@
+var Promise = require('promise'),
+    logger = require('lib/logger')(module);
+
+
+function _defaultUsers(models) {
+     return [
+         new models.User({ email: 'admin@admin.com', firstName: 'admin', secondName: 'admin' }),
+         new models.User({ email: 'user@user.com', firstName: 'user', secondName: 'user' })
+     ];
+}
+
+/**
+ * Save all entities to db.
+ *
+ * @param entities {Array<Objects>} List of entities
+ * @returns {Promise}
+ * @private
+ */
+function _saveAll(entities) {
+    
+    var promises = [];
+
+    entities.forEach(function(item) {
+        promises.push(new Promise(function(resolve, reject){
+            item.save(function(err, obj){
+                err ? reject(err) : resolve(obj);
+            });
+        }));
+    });
+
+    return Promise.all(promises);
+}
+
+
+module.exports = {
+
+    /**
+     * Save to database default values.
+     *
+     * @param models
+     * @returns {Promise}
+     */
+    defaultDBState: function(models) {
+        if(!models) {
+            logger.warn('Models is undefined.');
+            return Promise.resolve();
+        }
+
+        logger.info('Try to save default entities to db.');
+        return Promise.all([
+            _saveAll(_defaultUsers(models))
+        ]);
+    }
+};
